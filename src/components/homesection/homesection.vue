f088ffb3299e3d05c3927b82e3621739741fb97f
easonhuang123 <eason_huang@kingdee.com>
2017-12-28 20:05:52 +0800

    swiper

--- src/components/homesection/homesection.vue
+++ src/components/homesection/homesection.vue
@@ -11,107 +11,34 @@
                 <li>情歌</li>
             </ul>
             <div class='section__playlist'>
-                <ul class='section__playlist__box' ref='list1'>
-                    <li class='section__playlist__list'>
-                        <div class='section__playlist__imgbox'>
-                            <img class='section__playlist__img' src="https://p.qpic.cn/music_cover/ibSiagqKjw1zfTgxY7F8CfE1dqXGAzzrA72wRZiay9klJib47ciaE0a50ibA/300?n=1" alt="">
-                            <i class='section__playlist__play'></i>
-                        </div>
-                        <h4 class='section__playlist__title'>每日新歌：权革性感音色！即将爆发传染</h4>
-                        <p class='section__playlist__sum'>播放量：134.3万</p>
-                    </li>
-                    <li class='section__playlist__list'>
-                        <div class='section__playlist__imgbox'>
-                            <img class='section__playlist__img' src="https://p.qpic.cn/music_cover/ibSiagqKjw1zfTgxY7F8CfE1dqXGAzzrA72wRZiay9klJib47ciaE0a50ibA/300?n=1" alt="">
-                            <i class='section__playlist__play'></i>
-                        </div>
-                        <h4 class='section__playlist__title'>每日新歌：权革性感音色！即将爆发传染</h4>
-                        <p class='section__playlist__sum'>播放量：134.3万</p>
-                    </li>
-                    <li class='section__playlist__list'>
-                        <div class='section__playlist__imgbox'>
-                            <img class='section__playlist__img' src="https://p.qpic.cn/music_cover/ibSiagqKjw1zfTgxY7F8CfE1dqXGAzzrA72wRZiay9klJib47ciaE0a50ibA/300?n=1" alt="">
-                            <i class='section__playlist__play'></i>
-                        </div>
-                        <h4 class='section__playlist__title'>每日新歌：权革性感音色！即将爆发传染</h4>
-                        <p class='section__playlist__sum'>播放量：134.3万</p>
-                    </li>
-                    <li class='section__playlist__list'>
-                        <div class='section__playlist__imgbox'>
-                            <img class='section__playlist__img' src="https://p.qpic.cn/music_cover/ibSiagqKjw1zfTgxY7F8CfE1dqXGAzzrA72wRZiay9klJib47ciaE0a50ibA/300?n=1" alt="">
-                            <i class='section__playlist__play'></i>
-                        </div>
-                        <h4 class='section__playlist__title'>每日新歌：权革性感音色！即将爆发传染</h4>
-                        <p class='section__playlist__sum'>播放量：134.3万</p>
-                    </li>
-                    <li class='section__playlist__list'>
-                        <div class='section__playlist__imgbox'>
-                            <img class='section__playlist__img' src="https://p.qpic.cn/music_cover/ibSiagqKjw1zfTgxY7F8CfE1dqXGAzzrA72wRZiay9klJib47ciaE0a50ibA/300?n=1" alt="">
-                            <i class='section__playlist__play'></i>
-                        </div>
-                        <h4 class='section__playlist__title'>每日新歌：权革性感音色！即将爆发传染</h4>
-                        <p class='section__playlist__sum'>播放量：134.3万</p>
-                    </li>
-                    <li class='section__playlist__list'>
-                        <div class='section__playlist__imgbox'>
-                            <img class='section__playlist__img' src="https://p.qpic.cn/music_cover/ibSiagqKjw1zfTgxY7F8CfE1dqXGAzzrA72wRZiay9klJib47ciaE0a50ibA/300?n=1" alt="">
-                            <i class='section__playlist__play'></i>
-                        </div>
-                        <h4 class='section__playlist__title'>每日新歌：权革性感音色！即将爆发传染</h4>
-                        <p class='section__playlist__sum'>播放量：134.3万</p>
-                    </li>
-                    <li class='section__playlist__list'>
-                        <div class='section__playlist__imgbox'>
-                            <img class='section__playlist__img' src="https://p.qpic.cn/music_cover/ibSiagqKjw1zfTgxY7F8CfE1dqXGAzzrA72wRZiay9klJib47ciaE0a50ibA/300?n=1" alt="">
-                            <i class='section__playlist__play'></i>
-                        </div>
-                        <h4 class='section__playlist__title'>每日新歌：权革性感音色！即将爆发传染</h4>
-                        <p class='section__playlist__sum'>播放量：134.3万</p>
-                    </li>
-                    <li class='section__playlist__list'>
-                        <div class='section__playlist__imgbox'>
-                            <img class='section__playlist__img' src="https://p.qpic.cn/music_cover/ibSiagqKjw1zfTgxY7F8CfE1dqXGAzzrA72wRZiay9klJib47ciaE0a50ibA/300?n=1" alt="">
-                            <i class='section__playlist__play'></i>
-                        </div>
-                        <h4 class='section__playlist__title'>每日新歌：权革性感音色！即将爆发传染</h4>
-                        <p class='section__playlist__sum'>播放量：134.3万</p>
-                    </li>
-                    <li class='section__playlist__list'>
-                        <div class='section__playlist__imgbox'>
-                            <img class='section__playlist__img' src="https://p.qpic.cn/music_cover/ibSiagqKjw1zfTgxY7F8CfE1dqXGAzzrA72wRZiay9klJib47ciaE0a50ibA/300?n=1" alt="">
-                            <i class='section__playlist__play'></i>
-                        </div>
-                        <h4 class='section__playlist__title'>每日新歌：权革性感音色！即将爆发传染</h4>
-                        <p class='section__playlist__sum'>播放量：134.3万</p>
-                    </li>
-                    <li class='section__playlist__list section__playlist__list__last'>
-                        <div class='section__playlist__imgbox'>
-                            <img class='section__playlist__img' src="https://p.qpic.cn/music_cover/ibSiagqKjw1zfTgxY7F8CfE1dqXGAzzrA72wRZiay9klJib47ciaE0a50ibA/300?n=1" alt="">
-                            <i class='section__playlist__play'></i>
-                        </div>
-                        <h4 class='section__playlist__title'>每日新歌：权革性感音色！即将爆发传染</h4>
-                        <p class='section__playlist__sum'>播放量：134.3万</p>
+                <ul class='section__playlist__box' ref='list'>
+                    <li class='section__playlist__list' v-for="(item, index) in items" :key="index">
+                        <ul>
+                            <li class='section__playlist__inline_list' v-for="(i, idx) in item" :key="idx">
+                                <recommand :item='i'></recommand>
+                            </li>
+                        </ul>
                     </li>
                 </ul>
             </div>
             <div class='section__switch'>
                 <button class='section__switch__btn'
-                    :class='{section__switch__active: isSelected[0]}'
-                    @click="select(0, 'list1')">
+                    :class='{section__switch__active: position === 0 || position === -200}'
+                    @click="select(0)">
                 </button>
                 <button class='section__switch__btn'
-                    :class='{section__switch__active: isSelected[1]}'
-                    @click="select(1, 'list1')">
+                    :class='{section__switch__active: position === -100 || position === -300}'
+                    @click="select(1)">
                 </button>
             </div>
         </div>
         <transition name='recommand_show1'>
-            <div class='section__slide section__slide__left' v-show="recommand_show" @click="slide(1, 'list1')">
+            <div class='section__slide section__slide__left' v-show="recommand_show" @click="slide(1)">
                 <i class='section__slide__left__icon'></i>
             </div>
         </transition>
         <transition name='recommand_show2'>
-            <div class='section__slide section__slide__right' v-show="recommand_show" @click="slide(-1,'list1')">
+            <div class='section__slide section__slide__right' v-show="recommand_show" @click="slide(-1)">
                 <i class='section__slide__right__icon'></i>
             </div>
         </transition>
@@ -119,35 +46,150 @@
 </template>
 
 <script>
+import recommand from './recommand.vue'
 export default {
     name: 'homesection',
     data () {
         return {
-            isSelected: [1, 0],
             recommand_show: false,
-            left: -100
+            // 初始定位
+            position: -200,
+            items: [
+                [
+                    {
+                        src: 'https://p.qpic.cn/music_cover/UROl51JnIAk9Ipxmv4b86ia3hEvSxmWrN94e7uozJO7S02YeToKIIYQ/300?n=1',
+                        title: '你单曲循环的民谣，原来改编自外文歌 ',
+                        sum: '播放量：89.8万'
+                    },
+                    {
+                        src: 'https://p.qpic.cn/music_cover/ZxFhHIic15equyHQJ4akclTSufISZYtzTY7LUouxqjFo7nm68rC5vBA/300?n=1',
+                        title: '学会这50首成语歌名，你的粤语就冇问题了',
+                        sum: '播放量：11.3万'
+                    },
+                    {
+                        src: 'https://p.qpic.cn/music_cover/nnRNzNd5rZaggsDwk2uSnY4PVyZ1ZwLNT1ZrGJCSNgWBfQZWsGXU6A/300?n=1',
+                        title: '面试失利？听日系抒情摇滚找回自信',
+                        sum: '播放量：6.6万'
+                    },
+                    {
+                        src: 'https://p.qpic.cn/music_cover/lricrZ3ca62ybvZbWYjtYaicwxEBpDqdd9lm7dGhlZyUZscicDW9nJKUw/300?n=1',
+                        title: '期末复习：做复变函数的舒心曲',
+                        sum: '播放量：59.1万'
+                    },
+                    {
+                        src: 'https://p.qpic.cn/music_cover/etRVVF7aQu1fex4ZQECBHSERBDlhfv3mfPphFdIHAlDGckFLoHickqw/300?n=1',
+                        title: '空巢青年的苦涩：心就像无凑畔的孤岛 ',
+                        sum: '播放量：23.3万'
+                    }
+                ],
+                [
+                    
+                    {
+                        src: 'https://p.qpic.cn/music_cover/ibSiagqKjw1zfTgxY7F8CfE1dqXGAzzrA74s5eNwicVrvkv0tgvuyP3uw/300?n=1',
+                        title: '每日新歌：彭昱畅×孙雪宁诠释《爱刚刚好》 ',
+                        sum: '播放量：96.6万'
+                    },
+                    {
+                        src: 'https://p.qpic.cn/music_cover/1ZdGl7wveVA1a9fU8FWwvP4hLR5NFCZrjFJFJIT9F3pR2P7icQbDiblQ/300?n=1',
+                        title: '据说这些是2017年最受好评的专辑（一）',
+                        sum: '播放量：8.2万'
+                    },
+                    {
+                        src: 'https://p.qpic.cn/music_cover/8KfvDey9cibtZ5xkWxRic6vtgScu1rJ2j9TI2xszGYLx6PfthVdFq9Yg/300?n=1',
+                        title: '古韵佳人 | 淡眉如秋水，玉肌伴轻风 ',
+                        sum: '播放量：30.2万'
+                    },
+                    {
+                        src: 'https://p.qpic.cn/music_cover/0XaJ7WwKJXDzVrtkNbdjrT2NSL5sdBcibgL152mIlFNCtktu2hkgqZg/300?n=1',
+                        title: 'B榜评选：史上最伟大R&B歌手Top35',
+                        sum: '播放量：12.6万'
+                    },
+                    {
+                        src: 'https://p.qpic.cn/music_cover/UYaunx856XZrhEh6IpyK6qPE3mVlk2JpIKYLjQajHtgHyglWzN3iceg/300?n=1',
+                        title: '剧情控 | 百首日系视觉小说BGM ',
+                        sum: '播放量：10.4万'
+                    }
+                ],
+                [
+                    {
+                        src: 'https://p.qpic.cn/music_cover/UROl51JnIAk9Ipxmv4b86ia3hEvSxmWrN94e7uozJO7S02YeToKIIYQ/300?n=1',
+                        title: '你单曲循环的民谣，原来改编自外文歌 ',
+                        sum: '播放量：89.8万'
+                    },
+                    {
+                        src: 'https://p.qpic.cn/music_cover/ZxFhHIic15equyHQJ4akclTSufISZYtzTY7LUouxqjFo7nm68rC5vBA/300?n=1',
+                        title: '学会这50首成语歌名，你的粤语就冇问题了',
+                        sum: '播放量：11.3万'
+                    },
+                    {
+                        src: 'https://p.qpic.cn/music_cover/nnRNzNd5rZaggsDwk2uSnY4PVyZ1ZwLNT1ZrGJCSNgWBfQZWsGXU6A/300?n=1',
+                        title: '面试失利？听日系抒情摇滚找回自信',
+                        sum: '播放量：6.6万'
+                    },
+                    {
+                        src: 'https://p.qpic.cn/music_cover/lricrZ3ca62ybvZbWYjtYaicwxEBpDqdd9lm7dGhlZyUZscicDW9nJKUw/300?n=1',
+                        title: '期末复习：做复变函数的舒心曲',
+                        sum: '播放量：59.1万'
+                    },
+                    {
+                        src: 'https://p.qpic.cn/music_cover/etRVVF7aQu1fex4ZQECBHSERBDlhfv3mfPphFdIHAlDGckFLoHickqw/300?n=1',
+                        title: '空巢青年的苦涩：心就像无凑畔的孤岛 ',
+                        sum: '播放量：23.3万'
+                    }
+                ],
+                [
+                    
+                    {
+                        src: 'https://p.qpic.cn/music_cover/ibSiagqKjw1zfTgxY7F8CfE1dqXGAzzrA74s5eNwicVrvkv0tgvuyP3uw/300?n=1',
+                        title: '每日新歌：彭昱畅×孙雪宁诠释《爱刚刚好》 ',
+                        sum: '播放量：96.6万'
+                    },
+                    {
+                        src: 'https://p.qpic.cn/music_cover/1ZdGl7wveVA1a9fU8FWwvP4hLR5NFCZrjFJFJIT9F3pR2P7icQbDiblQ/300?n=1',
+                        title: '据说这些是2017年最受好评的专辑（一）',
+                        sum: '播放量：8.2万'
+                    },
+                    {
+                        src: 'https://p.qpic.cn/music_cover/8KfvDey9cibtZ5xkWxRic6vtgScu1rJ2j9TI2xszGYLx6PfthVdFq9Yg/300?n=1',
+                        title: '古韵佳人 | 淡眉如秋水，玉肌伴轻风 ',
+                        sum: '播放量：30.2万'
+                    },
+                    {
+                        src: 'https://p.qpic.cn/music_cover/0XaJ7WwKJXDzVrtkNbdjrT2NSL5sdBcibgL152mIlFNCtktu2hkgqZg/300?n=1',
+                        title: 'B榜评选：史上最伟大R&B歌手Top35',
+                        sum: '播放量：12.6万'
+                    },
+                    {
+                        src: 'https://p.qpic.cn/music_cover/UYaunx856XZrhEh6IpyK6qPE3mVlk2JpIKYLjQajHtgHyglWzN3iceg/300?n=1',
+                        title: '剧情控 | 百首日系视觉小说BGM ',
+                        sum: '播放量：10.4万'
+                    }
+                ]
+            ]
         }
     },
+    components: { recommand },
     methods: {
+        // 点击下面的小点点切换
         select (num, list) {
-            if (!this.isSelected[num]) {
-                for (let i = 0; i < this.isSelected.length; i++) {
-                    if (i === num) {
-                        this.isSelected[i] = 1
-                    } else {
-                        this.isSelected[i] = 0
-                    }
-                }
-                this.$refs[list].style.left = `${(num - 1) * 100}%`
-            }
+            this.position = (num - 2) * 100
+            this.$refs.list.style.transition = 'left .6s ease-out'
+            this.$refs.list.style.left = `${this.position}%`
         },
+        // 点击左右切换
+        // mask 1:左切换 -1:右切换
+        // list 目标轮播列表
         slide (mask, list) {
-            if (mask === 1 && this.left > -100) {
-                this.left -= 100
-                this.$refs[list].style.left = `${this.left}%`
-            } else if (mask === -1 && this.left < 0) {
-                this.left += 100
-                this.$refs[list].style.left = `${this.left}%`
+            this.position += mask * 100
+            this.$refs.list.style.transition = 'left .6s ease-out'
+            this.$refs.list.style.left = `${this.position}%`
+            // 到达临界点时转换位置并改变动画效果
+            if (this.position === 0 || this.position === -300) {
+                this.position += mask * -1 * 200
+                setTimeout(() => {
+                    this.$refs.list.style.transition = 'none'
+                    this.$refs.list.style.left = `${this.position}%`
+                }, 600)
             }
         }
     }
@@ -195,43 +237,18 @@ export default {
             min-width: 900px;
             .section__playlist__box{
                 position: relative;
-                left: -100%;
-                width: 200%;
-                transition: left .6s ease-out;
+                width: 401.7%;
+                left: -200%;
                 .section__playlist__list{
                     list-style: none;
                     display: inline-block;
-                    width: 9.25%;
-                    margin-right: .56%;
-                    .section__playlist__imgbox{
+                    width: 24.8%;
+                    .section__playlist__inline_list{
+                        list-style: none;
                         display: inline-block;
-                        margin-bottom: 15px;
-                        .section__playlist__img{
-                            width: 100%;
-                        }  
+                        width: 17.3%;
+                        margin: 0 1.1%;
                     }
-                    .section__playlist__title{
-                        font-size: 14px;
-                        font-weight:normal;
-                        margin-bottom: 5px;
-                        overflow: hidden;
-                        text-overflow:ellipsis;
-                        line-height: 22px;
-                        max-height: 44px;
-                        cursor: pointer;
-                        &:hover{
-                            color: #31c27c;
-                        }
-                    }
-                    .section__playlist__sum{
-                        font-size: 14px;
-                        color: #999;
-                        white-space: nowrap;
-                        text-overflow: ellipsis;
-                    }
-                }
-                .section__playlist__list__last{
-                    margin-right: 0;
                 }
             }
         }
---
+++ src/components/homesection/recommand.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <div class='section__playlist__imgbox'>
+            <img class='section__playlist__img' :src="item.src" alt="">
+            <i class='section__playlist__play'></i>
+        </div>
+        <h4 class='section__playlist__title'>{{item.title}}</h4>
+        <p class='section__playlist__sum'>{{item.sum}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'recommand',
+    props: [ 'item' ]
+}
+</script>
+
+<style lang="less">
+.section__playlist__imgbox{
+    position: relative;
+    margin-bottom: 15px;
+    overflow: hidden;
+    &:before {
+        content: " ";
+        display: block;
+        padding-top: 100%; /*利用padding来撑开*/
+    }
+    .section__playlist__img{
+        position: absolute;
+        top: 0; bottom: 0; right: 0; left: 0;
+        width: 100%;
+        transform: scale(1);
+        transition: transform .75s;
+        &:hover {
+            transform: scale(1.07);
+            transition: transform .75s cubic-bezier(0,1,0.75,1);
+        }
+    }  
+}
+.section__playlist__title{
+    font-size: 14px;
+    font-weight:normal;
+    margin-bottom: 5px;
+    overflow: hidden;
+    text-overflow:ellipsis;
+    line-height: 22px;
+    max-height: 44px;
+    cursor: pointer;
+    &:hover{
+        color: #31c27c;
+    }
+}
+.section__playlist__sum{
+    font-size: 14px;
+    color: #999;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+}   
+</style>
